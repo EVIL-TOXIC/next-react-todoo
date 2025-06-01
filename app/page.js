da89547161cf455e3327192ec3057e37c1e555eb
💭💭 TROFF JUNKIE 💭💭 <tusharbisht706@gmail.com>
2025-06-02 01:44:24 +0530

    commit-master

--- app/page.js
+++ app/page.js
@@ -1,103 +1,65 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
 
-export default function Home() {
+const page = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [maintask, setmaintask] = useState([]);
+  const submithandler = (e) => {
+    e.preventDefault();
+    setmaintask([...maintask, { title, desc }]);
+    setTitle("");
+    setDesc("");
+  };
+  const deleteHandler = (i) => {
+    let copytask = [...maintask];
+    copytask.splice(i,1)
+    setmaintask(copytask);
+    
+  }
+  let rendertask = <h2 className="text-4xl">🙄🙄 NO TASK ADDED 🙄🙄</h2>;
+  if(maintask.length > 0) {
+    rendertask = maintask.map((task, index) => (
+      <li key = {index} className="text-2xl m-2 flex items-center justify-between">
+        <h4 className="text-3xl font-bold">{task.title}</h4>
+        <h5 className="text-3xl font-bold">{task.desc}</h5>
+        <button onClick= {(index) => deleteHandler(index)}
+          className="bg-red-700 text-white px-4 py-2 m-2 rounded-tr-xl rounded-bl-xl hover:bg-red-50 text-2xl font-bold">
+          DELETE
+        </button>
+      </li>
+    ));
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <h1 className="bg-gradient-to-r from-slate-900 to-gray-900 text-white p-5 text-6xl text-center">
+        💭💭THIS IS MY TODOO LIST💭💭
+      </h1>
+      <form className="text-center m-5" onSubmit={submithandler}>
+        <input
+          type="text"
+          placeholder="Enter title here"
+          className="text-2xl border-zinc-900 border-2 m-5 px-4 py-2 bo"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <button className="bg-black text-white px-4 py-2 text-xl text-bold rounded-tr-xl rounded-bl-xl hover:bg-black/50">  
+          Add task
+        </button>
+        <input
+          type="text"
+          placeholder="Enter description here"
+          className="text-2xl border-zinc-900 border-2 m-5 px-4 py-2"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+      </form>
+      <hr />
+      <div className="p-8 bg-gradient-to-r from-gray-900 to-gray-600 text-white text-center">
+        <ul>{rendertask}</ul>
+      </div>
+    </>
   );
-}
+};
+
+export default page;
